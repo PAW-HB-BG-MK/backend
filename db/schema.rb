1708425ef281d580e645f84dbe4ad398e2591501
Graleszczur <gralewski.bartosz@gmail.com>
2020-11-18 11:59:28 +0100

    recreate db schema in rails api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_180956) do
+ActiveRecord::Schema.define(version: 2020_11_18_104610) do
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "pongs", force: :cascade do |t|
     t.string "pong"
@@ -18,4 +26,12 @@ ActiveRecord::Schema.define(version: 2020_11_03_180956) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "boards", "users"
 end
