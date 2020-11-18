6f6b5c5e126ea2e54062c414e41c26204afcd6e8
Bartosz Gralewski <gralewski.bartosz@gmail.com>
2020-11-18 16:51:05 +0100

    add boards related endpoints (#6)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :boards
   has_secure_password
   validates :email, presence: true,uniqueness: true
+  def to_token_payload
+    {
+        sub: id,
+        email: email
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
   scope '/auth' do
     post '/sign_in', to: 'user_token#create'
+  end
+  scope '/api' do
+    get '/boards', to: 'boards#get_boards'
+    post '/board/add', to: 'boards#add_board'
   end
   namespace 'api' do
     namespace 'v1' do
