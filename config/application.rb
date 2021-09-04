fa50c1dc084f6301c0a58d2f7362a3ca56a8cf32
Arrlen <rozwodniony1@gmail.com>
2021-09-04 23:13:54 +0200

    allow all through cors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module TrelloClone
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => :any
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'localhost', '127.0.0.1', 'paw.rehost.pl'
+     origins '*'
 
      resource '*',
        headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+       methods: :any
    end
 end
